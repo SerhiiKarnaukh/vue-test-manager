5f152458ce472b0ed1f8ca0428858ee0426f670d
DESKTOP-O40LJJ8\serhi <serhiikarnaukh1984@gmail.com>
2022-12-02 20:18:18 +0200

    Include Font Awesome

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,19 +1,15 @@
-/**
- * plugins/vuetify.js
- *
- * Framework documentation: https://vuetifyjs.com`
- */
 
-// Styles
+import '@fortawesome/fontawesome-free/css/all.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
-// Composables
 import { createVuetify } from 'vuetify'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    icons: {
+      iconfont: 'md' || 'fa' || 'md'
+
+    },
     themes: {
       light: {
         colors: {

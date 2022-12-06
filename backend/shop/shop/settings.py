ee4d815a83f2e0a5e38c64ad25d933206f480174
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2022-12-06 16:32:32 +0200

    Create django app and models for products

--- backend/shop/shop/settings.py
+++ backend/shop/shop/settings.py
@@ -34,16 +34,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'corsheaders',
-    'djoser',
+    'django.contrib.admin', 'django.contrib.auth',
+    'django.contrib.contenttypes', 'django.contrib.sessions',
+    'django.contrib.messages', 'django.contrib.staticfiles', 'rest_framework',
+    'rest_framework.authtoken', 'corsheaders', 'djoser', 'product'
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -128,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

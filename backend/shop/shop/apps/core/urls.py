f5a6985701f2389c643b1dceccad4e3a635390a2
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2023-01-05 18:43:27 +0200

    Changed Store Page

--- backend/shop/shop/apps/core/urls.py
+++ backend/shop/shop/apps/core/urls.py
@@ -4,6 +4,7 @@ from core.views import contact, about
 from product.views import ProductDetail, CategoryDetail
 
 urlpatterns = [
+    path('', CategoryDetail.as_view(), name='store'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('<slug:slug>/', CategoryDetail.as_view(), name='category_detail'),
--- backend/shop/shop/apps/product/views.py
+++ backend/shop/shop/apps/product/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 
 from .models import Product, Category
 from .serializers import ProductSerializer
@@ -18,18 +18,39 @@ class ProductDetail(DetailView):
         return context
 
 
-class CategoryDetail(DetailView):
-    model = Category
-    template_name = 'product/category_detail.html'
+class CategoryDetail(ListView):
+    model = Product
+    template_name = 'product/store.html'
     context_object_name = 'products'
     allow_empty = False
 
+    def create_store_data(self, **kwargs):
+        context = kwargs
+        if 'slug' in self.kwargs:
+            context['store_title'] = Category.objects.get(
+                slug=self.kwargs['slug'])
+            context['product_count'] = Product.objects.filter(
+                category__slug=self.kwargs['slug'], is_available=True).count()
+            return context
+        else:
+            context['store_title'] = 'All products'
+            context['product_count'] = Product.objects.all().filter(
+                is_available=True).count()
+            return context
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products'] = Product.objects.filter(
-            category__slug=self.kwargs['slug']).select_related('category')
-        context['category'] = Category.objects.get(slug=self.kwargs['slug'])
-        return context
+        c_def = self.create_store_data()
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self, **kwargs):
+        if 'slug' in self.kwargs:
+            return Product.objects.filter(
+                category__slug=self.kwargs['slug'],
+                is_available=True).select_related('category')
+        else:
+            return Product.objects.all().filter(
+                is_available=True).select_related('category')
 
 
 # for Django Rest Framework

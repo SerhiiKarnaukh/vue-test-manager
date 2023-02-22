dc3a2671c8e138159c53f08a7e38adbe211d61cc
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2023-02-22 13:09:15 +0200

    Variations added

--- backend/shop/shop/apps/cart/urls.py
+++ backend/shop/shop/apps/cart/urls.py
@@ -4,8 +4,10 @@ from .views import cart, add_cart, remove_cart, remove_cart_item
 urlpatterns = [
     path('', cart, name='cart'),
     path('add_cart/<int:product_id>/', add_cart, name='add_cart'),
-    path('remove_cart/<int:product_id>/', remove_cart, name='remove_cart'),
-    path('remove_cart_item/<int:product_id>/',
+    path('remove_cart/<int:product_id>/<int:cart_item_id>/',
+         remove_cart,
+         name='remove_cart'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/',
          remove_cart_item,
          name='remove_cart_item'),
 ]
--- backend/shop/shop/apps/product/admin.py
+++ backend/shop/shop/apps/product/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Product
+from .models import Category, Product, Variation
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -21,5 +21,13 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name", )}
 
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value',
+                    'is_active')
+    list_editable = ('is_active', )
+    list_filter = ('product', 'variation_category', 'variation_value')
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)

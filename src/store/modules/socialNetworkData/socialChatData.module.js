b42d3d0540d5e45e885946e862358ad4333e0df1
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2024-02-13 22:18:27 +0200

    Fix: social chat

--- src/store/modules/socialNetworkData/socialChatData.module.js
+++ src/store/modules/socialNetworkData/socialChatData.module.js
@@ -1,8 +1,22 @@
 import axios from 'axios'
 
-const state = () => ({})
+const state = () => ({
+  conversations: [],
+  activeConversation: {},
+})
 
-const mutations = {}
+const mutations = {
+  clearChatData(state) {
+    state.conversations = []
+    state.activeConversation = {}
+  },
+  setActiveConversation(state, id) {
+    state.activeConversation = id
+  },
+  setConversations(state, items) {
+    state.conversations = items
+  },
+}
 
 const actions = {
   async getOrCreateChat({ dispatch }, userSlug) {
@@ -20,9 +34,47 @@ const actions = {
       return Promise.reject(error)
     }
   },
+  async getChatMessages({ state, commit }) {
+    try {
+      const response = await axios.get(
+        `/api/social-chat/${state.activeConversation}/`
+      )
+      commit('setActiveConversation', response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  async getConversations({ state, commit, dispatch }) {
+    try {
+      const response = await axios.get('/api/social-chat/')
+      commit('setConversations', response.data)
+      if (state.conversations.length) {
+        commit('setActiveConversation', state.conversations[0].id)
+      }
+      dispatch('getChatMessages')
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  async submitChatForm({ state }, message) {
+    try {
+      const response = await axios.post(
+        `/api/social-chat/${state.activeConversation.id}/send/`,
+        {
+          body: message,
+        }
+      )
+      state.activeConversation.messages.push(response.data)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  },
 }
 
-const getters = {}
+const getters = {
+  conversations: (state) => state.conversations,
+  activeConversation: (state) => state.activeConversation,
+}
 
 export default {
   namespaced: true,
--- src/views/social/ChatView.vue
+++ src/views/social/ChatView.vue
@@ -7,25 +7,28 @@
             v-for="conversation in conversations"
             v-bind:key="conversation.id"
             v-on:click="setActiveConversation(conversation.id)"
+            :class="{
+              'active-conversation': conversation.id === activeConversation.id,
+            }"
             class="px-4 py-6 mb-3 rounded-lg"
           >
-            <v-row align="center">
-              <v-col
-                class="shrink-0"
-                v-for="user in conversation.users"
-                v-bind:key="user.id"
-              >
+            <v-row
+              align="center"
+              v-for="user in conversation.users"
+              v-bind:key="user.id"
+            >
+              <v-col cols="3">
                 <v-avatar size="50" v-if="user.id != userId">
                   <img :src="user.avatar_url" style="max-width: 100%"
                 /></v-avatar>
               </v-col>
-              <v-row v-for="user in conversation.users" v-bind:key="user.id">
+              <v-col cols="5">
                 <p class="text-xs" v-if="user.id != userId">
                   <strong>{{ user.first_name + ' ' + user.last_name }}</strong>
                 </p>
-              </v-row>
-              <v-col class="shrink-0">
-                <span class="text-xs text-gray-500"
+              </v-col>
+              <v-col cols="4">
+                <span v-if="user.id != userId" class="text-xs text-gray-500"
                   >{{ conversation.modified_at_formatted }} ago</span
                 >
               </v-col>
@@ -34,18 +37,46 @@
         </v-col>
 
         <v-col cols="12" md="5" lg="6" class="px-4">
-          <v-card class="mb-6 pa-4 rounded-lg">
+          <v-card
+            v-if="
+              activeConversation.messages &&
+              activeConversation.messages.length !== 0
+            "
+            class="mb-6 pa-4 rounded-lg"
+          >
             <v-list>
               <v-list-item
                 v-for="message in activeConversation.messages"
                 :key="message.id"
                 :class="getMessageClass(message)"
               >
-                <v-avatar size="50">
-                  <img
-                    :src="message.created_by.avatar_url"
-                    style="max-width: 100%"
-                /></v-avatar>
+                <v-row align="center">
+                  <v-col
+                    :class="{
+                      'order-2': getMessageClass(message) === 'sender-message',
+                      'order-1':
+                        getMessageClass(message) === 'receiver-message',
+                    }"
+                    cols="5"
+                  >
+                    <v-avatar size="50">
+                      <img
+                        :src="message.created_by.avatar_url"
+                        style="max-width: 100%"
+                    /></v-avatar>
+                  </v-col>
+                  <v-col
+                    :class="{
+                      'order-1': getMessageClass(message) === 'sender-message',
+                      'order-2':
+                        getMessageClass(message) === 'receiver-message',
+                    }"
+                    cols="7"
+                  >
+                    <strong>{{ message.created_by.first_name }}</strong>
+                  </v-col>
+                </v-row>
+
                 <v-card-text>
                   <v-list-item-title>
                     {{ message.body }}
@@ -61,7 +92,7 @@
             <v-form v-on:submit.prevent="submitForm" method="post">
               <v-card-text class="p-4">
                 <v-textarea
-                  v-model.trim="body"
+                  v-model.trim="state.body"
                   placeholder="What do you want to say?"
                   color="grey lighten-3"
                   required
@@ -81,89 +112,69 @@
   </v-main>
 </template>
 <script>
-import axios from 'axios'
-import { mapGetters } from 'vuex'
+import { reactive, onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
-  name: 'СhatView',
+  name: 'ChatView',
 
-  setup() {},
-
-  data() {
-    return {
-      conversations: [],
-      activeConversation: {},
+  setup() {
+    const store = useStore()
+    const state = reactive({
       body: '',
-    }
-  },
+    })
 
-  mounted() {
-    document.title = 'Chat | Social Network'
-    this.getConversations()
-  },
-  computed: {
-    ...mapGetters('socialProfileData', {
-      userId: 'userId',
-    }),
-  },
+    const conversations = computed(() => {
+      return store.getters['socialChatData/conversations']
+    })
 
-  methods: {
-    getMessageClass(message) {
-      return message.created_by.id == this.userId
+    const activeConversation = computed(() => {
+      return store.getters['socialChatData/activeConversation']
+    })
+
+    const userId = computed(() => store.getters['socialProfileData/userId'])
+
+    const getMessageClass = (message) => {
+      return message.created_by.id === userId.value
         ? 'sender-message'
         : 'receiver-message'
-    },
-    setActiveConversation(id) {
-      this.activeConversation = id
-      this.getMessages()
-    },
-    getConversations() {
-      axios
-        .get('/api/social-chat/')
-        .then((response) => {
-          this.conversations = response.data
+    }
 
-          if (this.conversations.length) {
-            this.activeConversation = this.conversations[0].id
-          }
+    const setActiveConversation = async (id) => {
+      store.commit('socialChatData/setActiveConversation', id)
+      await store.dispatch('socialChatData/getChatMessages')
+    }
 
-          this.getMessages()
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
+    const submitForm = async () => {
+      try {
+        await store.dispatch('socialChatData/submitChatForm', state.body)
+        state.body = ''
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-    getMessages() {
-      axios
-        .get(`/api/social-chat/${this.activeConversation}/`)
-        .then((response) => {
-          this.activeConversation = response.data
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
+    onMounted(async () => {
+      await store.dispatch('setPageTitle', 'Chat')
+      await store.dispatch('socialChatData/getConversations')
+    })
 
-    submitForm() {
-      axios
-        .post(`/api/social-chat/${this.activeConversation.id}/send/`, {
-          body: this.body,
-        })
-        .then((response) => {
-          console.log(response.data)
-
-          this.activeConversation.messages.push(response.data)
-          this.body = ''
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
+    return {
+      state,
+      conversations,
+      activeConversation,
+      userId,
+      getMessageClass,
+      setActiveConversation,
+      submitForm,
+    }
   },
 }
 </script>
 <style scoped>
+.active-conversation {
+  background-color: #e0e0e0;
+}
 .sender-message {
   text-align: right;
   background-color: #e0e0e0;
@@ -173,7 +184,6 @@ export default {
   max-width: 50%;
   margin-left: auto;
 }
-
 .receiver-message {
   text-align: left;
   background-color: #f5f5f5;

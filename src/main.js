b2f253490d174d11a20c549e5e647ff9b9a5ad32
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2025-04-29 20:18:28 +0300

    [appsmanager] - added Pinia

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import axios from 'axios'
 import './axios-interceptor'
+import { createPinia } from 'pinia'
 
 axios.defaults.baseURL = `${import.meta.env.VITE_REMOTE_HOST}`
 
 loadFonts()
 
-createApp(App).use(router).use(store).use(vuetify).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(vuetify)
+app.use(createPinia())
+app.mount('#app')

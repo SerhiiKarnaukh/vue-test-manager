892c90fb42f71059a04797d6019ff0a278c74c76
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2025-06-20 13:46:56 +0300

    [hyper3d] - added Light

--- src/views/hyper3d/HomeView.vue
+++ src/views/hyper3d/HomeView.vue
@@ -18,7 +18,29 @@ let scene, camera, renderer
 
 function renderObject() {
   container.value.innerHTML = ''
+  // Scene
   scene = new THREE.Scene()
+
+  // Light
+
+  //   const ambientLight = new THREE.AmbientLight('white', 1)
+  //   scene.add(ambientLight)
+
+  //   const dirLight = new THREE.DirectionalLight('white', 1)
+  //   dirLight.position.set(5, 5, 5)
+  //   scene.add(dirLight)
+
+  //   const pointLight = new THREE.PointLight('white', 10, 100)
+  //   pointLight.position.set(-1, 1, 1)
+  //   scene.add(pointLight)
+  //   const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5)
+  //   scene.add(pointLightHelper)
+
+  const spotLight = new THREE.SpotLight('white', 6)
+  spotLight.position.set(1, 1, 1)
+  scene.add(spotLight)
+
+  // Camera
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -26,15 +48,20 @@ function renderObject() {
     100
   )
   camera.position.z = 5
+
+  // Renderer
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
   container.value.appendChild(renderer.domElement)
+
+  // Cube
   const geometry = new THREE.BoxGeometry()
-  const material = new THREE.MeshBasicMaterial({ color: 'green' })
+  const material = new THREE.MeshStandardMaterial({ color: 'green' })
   const cube = new THREE.Mesh(geometry, material)
   cube.position.set(-3, 0, 0)
   scene.add(cube)
 
+  // Sphere
   const sphereGeometry = new THREE.SphereGeometry(0.5, 6, 6)
   const sphereMaterial = new THREE.MeshPhongMaterial({
     color: 'orange',
@@ -45,6 +72,7 @@ function renderObject() {
   sphere.position.set(3, 0, 0)
   scene.add(sphere)
 
+  // Torus
   const torus = new THREE.Mesh(
     new THREE.TorusGeometry(0.7, 0.2, 16, 100),
     new THREE.MeshBasicMaterial({ color: 'blue' })
